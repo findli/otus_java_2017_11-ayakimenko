fd79ac28596c33dfc34530f91d68ecad4e50da01
abyakimenko <a.yakimenko@connectflexi.com>
2018-01-20 00:21:50 +0300

    - set compiler to jdk9 - disable proguard - #2 Memory meter added arrays measure

--- L2-Memory/src/main/java/ru/otus/L2/Main.java
+++ L2-Memory/src/main/java/ru/otus/L2/Main.java
@@ -1,5 +1,8 @@
 package ru.otus.L2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.lang.management.ManagementFactory;
 
 /**
@@ -15,9 +18,11 @@ import java.lang.management.ManagementFactory;
 @SuppressWarnings({"RedundantStringConstructorCall", "InfiniteLoopStatement"})
 public class Main {
 
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String... args) throws InterruptedException {
 
-        System.out.println("pid: " + ManagementFactory.getRuntimeMXBean().getName());
+        logger.info("pid: {}", ManagementFactory.getRuntimeMXBean().getName());
 
         MemoryMeter memoryMeter = new MemoryMeter();
         memoryMeter.setUp();
@@ -29,6 +34,26 @@ public class Main {
         memoryMeter.clean();
 
         memoryMeter.measure(() -> new String(new char[10]), "Empty String pool free");
+        memoryMeter.clean();
+        
+        logger.info("Testing with different array size");
+
+        memoryMeter.measure(() -> new int[0], "Int array, 0 elements");
+        memoryMeter.clean();
+
+        memoryMeter.measure(() -> new int[10], "Int array, 10 elements");
+        memoryMeter.clean();
+
+        memoryMeter.measure(() -> new int[100], "Int array, 100 elements");
+        memoryMeter.clean();
+
+        memoryMeter.measure(() -> new int[1000], "Int array, 1000 elements");
+        memoryMeter.clean();
+
+        memoryMeter.measure(() -> new int[10_000], "Int array, 10_000 elements");
+        memoryMeter.clean();
+
+        memoryMeter.measure(() -> new int[100_000], "Int array, 100_000 elements");
         memoryMeter.clean();
     }
 }
--- L2-Memory/src/main/java/ru/otus/L2/MemoryMeter.java
+++ L2-Memory/src/main/java/ru/otus/L2/MemoryMeter.java
@@ -1,27 +1,32 @@
 package ru.otus.L2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
 /**
  * Created by abyakimenko on 29.11.2017.
- *
+ * <p>
  * For 64 bit VMs there are options:
-
-    1.Using compressed pointers via -XX:+UseCompressedOops (enabled by default on Java 6)
-    In that case: object headers will be 12 bytes, array headers will be 16 bytes (last 4 byte for size of array)
-
-    2.Not using compressed pointers via -XX:-UseCompressedOops
-
-    In that case: object headers will be 16 bytes, array headers will be 20 bytes (last 4 byte for size of array)
+ * <p>
+ * 1.Using compressed pointers via -XX:+UseCompressedOops (enabled by default on Java 6)
+ * In that case: object headers will be 12 bytes, array headers will be 16 bytes (last 4 byte for size of array)
+ * <p>
+ * 2.Not using compressed pointers via -XX:-UseCompressedOops
+ * <p>
+ * In that case: object headers will be 16 bytes, array headers will be 20 bytes (last 4 byte for size of array)
  */
 public class MemoryMeter {
+
+    private static final Logger logger = LoggerFactory.getLogger(MemoryMeter.class);
 
     private Object[] array;
     private int size;
 
     public MemoryMeter() {
-        this.size = 10_000_000;
+        this.size = 1_000_000;
     }
 
     public MemoryMeter(int size) {
@@ -31,33 +36,32 @@ public class MemoryMeter {
 
     void setUp() throws InterruptedException {
         long memoryChanges = getMemChanges(() -> this.array = new Object[size]);
-        System.out.println("References size: " + memoryChanges);
+        logger.info("References size: {}", memoryChanges);
     }
 
     public void measure(Supplier<Object> supplier, String name) throws InterruptedException {
         long memoryChanges = getMemChanges(() -> {
-            int i = 0;
-            while (i < size) {
+            for (int i = 0; i < size; i++) {
                 array[i] = supplier.get();
-                ++i;
             }
         });
 
-        System.out.println("### name:" + name + ", size: " + Math.round((double) memoryChanges / size));
+        logger.info("### name:{} , size: {}", name, Math.round((double) memoryChanges / size));
     }
 
-    void clean() {
+    void clean() throws InterruptedException {
         array = new Object[size];
-        System.gc();
+        Runtime.getRuntime().gc();
+        TimeUnit.SECONDS.sleep(1); //wait for 1 sec
     }
 
     private long getMemChanges(Runnable creator) throws InterruptedException {
         Runtime runtime = Runtime.getRuntime();
-        System.gc();
+        runtime.gc();
         TimeUnit.SECONDS.sleep(1); //wait for 1 sec
         long memoryBefore = runtime.totalMemory() - runtime.freeMemory();
         creator.run();
-        System.gc();
+        runtime.gc();
         TimeUnit.SECONDS.sleep(1); //wait for 1 sec
         long memoryAfter = runtime.totalMemory() - runtime.freeMemory();
         return memoryAfter - memoryBefore;
